8b3e2c5a1a840152c78813af27fbd54770151b60
Joseph Paul <mail@jsph.pl>
2017-02-15 00:06:15 +0100

    Update build script: remove github pages target

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 ## Build the fabacademy docs to two different locations using different configs
 ##
 
-jekyll build -d ../fabgrid.github.io/
 jekyll build -b "http://archive.fabacademy.org/archives/2017/fablaberfindergarden/students/260" -d ../gitlab/students/260/
